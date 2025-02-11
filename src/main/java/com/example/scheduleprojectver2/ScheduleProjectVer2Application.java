344d1e15bb4c174e99d684a0782366f198c99ca3
mixedsider <dmstjd0812@gmail.com>
2025-02-11 16:44:05 +0900

    Initial commit

---
+++ src/main/java/com/example/scheduleprojectver2/ScheduleProjectVer2Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduleprojectver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleProjectVer2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduleprojectver2/ScheduleProjectVer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleprojectver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectVer2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
